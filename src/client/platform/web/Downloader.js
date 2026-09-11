31e13f715e9c6e87ce362f10eda38e156bef1e7e
Kintokikyo <abiksluke@gmail.com>
2026-09-11 10:08:08 +0700

    Update Downloader.js

--- src/client/platform/web/Downloader.js
+++ src/client/platform/web/Downloader.js
@@ -1,24 +1,81 @@
 import JSZip from 'jszip';
 import FileSaver from 'file-saver';
 
+
 class Downloader {
 
     static run(files, fileName) {
 
         let zip = new JSZip();
 
-        for(let file of files) {
-            zip.file(file.name, file.content, {base64: !!file.base64});
+
+        // =========================================================
+        // MASUKKAN SEMUA FILE KE ZIP
+        // =========================================================
+
+        for (let file of files) {
+
+            zip.file(
+                file.name,
+                file.content,
+                {
+                    base64: !!file.base64
+                }
+            );
         }
 
+
+        // =========================================================
+        // PASTIKAN EXTENSION .ZIP
+        // =========================================================
+
         let ext = fileName.split(".").pop();
-        if(ext !== "zip") fileName = fileName + ".zip";
 
-        zip.generateAsync({type:"blob"}).then((content) => {
-            FileSaver.saveAs(content, fileName);
-        });
+        if (ext !== "zip") {
+            fileName = fileName + ".zip";
+        }
+
+
+        // =========================================================
+        // ANDROID WEBVIEW
+        // =========================================================
+
+        if (
+            window.AndroidDownload &&
+            typeof window.AndroidDownload.saveBase64 === "function"
+        ) {
+
+            zip.generateAsync({
+                type: "base64"
+            }).then((content) => {
+
+                window.AndroidDownload.saveBase64(
+                    fileName,
+                    content
+                );
+
+            });
+
+
+        // =========================================================
+        // WEB BROWSER / CHROME
+        // =========================================================
+
+        } else {
+
+            zip.generateAsync({
+                type: "blob"
+            }).then((content) => {
+
+                FileSaver.saveAs(
+                    content,
+                    fileName
+                );
+
+            });
+        }
     }
-
 }
+
 
 export default Downloader;
